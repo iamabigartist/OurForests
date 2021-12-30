8d78bbb7bf17dc261ee6bbac7c3b0451e29210b8
iamabigartist <1548016008@qq.com>
2021-12-30 14:58:21 +0800

    Successfully reference Roslyn

---
+++ New Unity ProjectAutoMation&Data-Oriented Progamming Editor/Assets/Projects/Automation/TryRoslynServices.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Scripting;
+using UnityEditor.Build;
+using UnityEngine;
+namespace Projects.Automation
+{
+    public class TryRoslynServices : MonoBehaviour
+    {
+
+    }
+}
