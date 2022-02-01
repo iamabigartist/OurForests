eda6a4512517bca040e84de3f06c569b16316a94
iamabigartist <1548016008@qq.com>
2022-02-01 16:29:03 +0800

    clean

--- OF test0/Assets/Projects/RaycastingTest/CameraRayConclusion.cs
+++ OF test0/Assets/Projects/RaycastingTest/CameraRayConclusion.cs
@@ -3,9 +3,25 @@ namespace RaycastingTest
 {
     public class CameraRayConclusion : MonoBehaviour
     {
+
+        Resolution m_resolution;
+        Camera m_camera;
         void Start()
         {
-            var ray = Camera.main!.ScreenPointToRay( new Vector3( 0.5f, 0.5f, 0 ) );
+            m_resolution = Screen.currentResolution;
+            m_camera = Camera.main;
+        }
+        void OnDrawGizmos()
+        {
+
+            for (float i = 0; i < m_resolution.height; i++)
+            {
+                for (float j = 0; j < m_resolution.width; j++)
+                {
+                    Gizmos.DrawRay( m_camera.ScreenPointToRay(
+                        new Vector3( j / m_resolution.width, i / m_resolution.height, 0 ) ) );
+                }
+            }
         }
     }
 }
