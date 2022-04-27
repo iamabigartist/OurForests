e1957a0e3ed31877d1f778c2ce76c3a8661a2f1b
iamabigartist <pch4178@gmail.com>
2022-04-27 19:09:01 +0800

    integer style test

---
+++ New Unity ProjectAutoMation&Data-Oriented Progamming Editor/Assets/Projects/UI/UssLab/Editor/TestIntegerField.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+namespace UI.UssLab.Editor
+{
+	public class TestIntegerField : EditorWindow
+	{
+		[MenuItem("Labs/UI.UssLab.Editor/TestIntegerField")]
+		static void ShowWindow()
+		{
+			var window = GetWindow<TestIntegerField>();
+			window.titleContent = new("TestIntegerField");
+			window.Show();
+		}
+
+		void CreateGUI()
+		{
+			var integer_field = new IntegerField();
+			rootVisualElement.Add(integer_field );
+			integer_field.AddToClassList("my-integer-field");
+			var uss = Resources.Load<StyleSheet>("MyIntegerField");
+			rootVisualElement.styleSheets.Add(uss);
+			integer_field.value = 1023;
+		}
+	}
+}
