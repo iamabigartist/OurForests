b355bfb6efb6ea45146c0b4ed4b165111d069123
iamabigartist <pch4178@gmail.com>
2023-01-25 21:19:20 +0800

    using new buffer method, and fix previous greedy method

--- VolumeMegaStructure/Assets/Framework/Runtime/Scripts/Generate/ProceduralMesh/Voxel/SequentialDense/RectsGeneration.cs
+++ VolumeMegaStructure/Assets/Framework/Runtime/Scripts/Generate/ProceduralMesh/Voxel/SequentialDense/RectsGeneration.cs
@@ -47,7 +47,8 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 			where TIndexWalker : unmanaged, IIndexWalker
 		{
 			var walker = new TIndexWalker();
-			foreach (var (block_id, start_pos) in quad_set)
+			var quad_set_reader = quad_set.AsReadOnly();
+			foreach (var (block_id, start_pos) in quad_set_reader)
 			{
 				coordinate.To3D(start_pos, out var x, out var y, out var z);
 
@@ -55,15 +56,20 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				walker.Walk(ref beside_x, ref beside_y, ref beside_z, -1);
 				coordinate.To1D(beside_x, beside_y, beside_z, out var beside_pos);
 
-				if (quad_set.Contains(new(block_id, beside_pos))) { continue; }
+				if (!coordinate.OutOfRange(beside_x, beside_y, beside_z) &&
+					quad_set_reader.Contains(new(block_id, beside_pos)))
+				{
+					continue;
+				}
 
 				(beside_x, beside_y, beside_z) = (x, y, z);
 				var end_pos = start_pos;
 				while (true)
 				{
 					walker.Walk(ref beside_x, ref beside_y, ref beside_z, 1);
+					if (coordinate.OutOfRange(beside_x, beside_y, beside_z)) { break; }
 					coordinate.To1D(beside_x, beside_y, beside_z, out beside_pos);
-					if (!quad_set.Contains(new(block_id, beside_pos))) { break; }
+					if (!quad_set_reader.Contains(new(block_id, beside_pos))) { break; }
 					end_pos = beside_pos;
 				}
 				line_set.Add(new(block_id, start_pos, end_pos));
@@ -74,7 +80,8 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 			where TIndexWalker : unmanaged, IIndexWalker
 		{
 			var walker = new TIndexWalker();
-			foreach (var (block_id, start_pos, end_pos) in line_set)
+			var line_set_reader = line_set.AsReadOnly();
+			foreach (var (block_id, start_pos, end_pos) in line_set_reader)
 			{
 				coordinate.To3D(start_pos, out var start_x, out var start_y, out var start_z);
 				coordinate.To3D(end_pos, out var end_x, out var end_y, out var end_z);
@@ -87,7 +94,11 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				walker.Walk(ref beside_end_x, ref beside_end_y, ref beside_end_z, -1);
 				coordinate.To1D(beside_end_x, beside_end_y, beside_end_z, out var beside_end_pos);
 
-				if (line_set.Contains(new(block_id, beside_start_pos, beside_end_pos))) { return; }
+				if (!coordinate.OutOfRange(beside_start_x, beside_start_y, beside_start_z)
+					&& line_set_reader.Contains(new(block_id, beside_start_pos, beside_end_pos)))
+				{
+					continue;
+				}
 
 				(beside_start_x, beside_start_y, beside_start_z) = (start_x, start_y, start_z);
 				(beside_end_x, beside_end_y, beside_end_z) = (end_x, end_y, end_z);
@@ -95,36 +106,42 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				while (true)
 				{
 					walker.Walk(ref beside_start_x, ref beside_start_y, ref beside_start_z, 1);
+					if (coordinate.OutOfRange(beside_start_x, beside_start_y, beside_start_z)) { break; }
 					coordinate.To1D(beside_start_x, beside_start_y, beside_start_z, out beside_start_pos);
 					walker.Walk(ref beside_end_x, ref beside_end_y, ref beside_end_z, 1);
 					coordinate.To1D(beside_end_x, beside_end_y, beside_end_z, out beside_end_pos);
-					if (!line_set.Contains(new(block_id, beside_start_pos, beside_end_pos))) { break; }
+					if (!line_set_reader.Contains(new(block_id, beside_start_pos, beside_end_pos))) { break; }
 					rect_end_pos = beside_end_pos;
 				}
 				rect_set.Add(new(block_id, start_pos, rect_end_pos));
+
 			}
+
+
 		}
 
 		[BurstCompile(OptimizeFor = OptimizeFor.Performance, DisableSafetyChecks = true)]
 		public struct GenRectSetsJob : IJob, IPlan
 		{
+			[NoAlias] [ReadOnly] int inside_block_group;
+			[NoAlias] [ReadOnly] NativeArray<int> block_group_by_id;
+
 			[NoAlias] Index3D coordinate;
-			[NoAlias] [ReadOnly] NativeArray<ushort> volume_chunk;
-			[NoAlias] [ReadOnly] NativeArray<ushort> volume_chunk_neighbor_x;
-			[NoAlias] [ReadOnly] NativeArray<ushort> volume_chunk_neighbor_y;
-			[NoAlias] [ReadOnly] NativeArray<ushort> volume_chunk_neighbor_z;
-			[NoAlias] [ReadOnly] NativeArray<bool> inside_chunk;
-			[NoAlias] [ReadOnly] NativeArray<bool> inside_chunk_neighbor_x;
-			[NoAlias] [ReadOnly] NativeArray<bool> inside_chunk_neighbor_y;
-			[NoAlias] [ReadOnly] NativeArray<bool> inside_chunk_neighbor_z;
+			[NoAlias] [ReadOnly] NativeArray<ushort> block_chunk;
+			[NoAlias] [ReadOnly] NativeArray<ushort> block_chunk_forward_x;
+			[NoAlias] [ReadOnly] NativeArray<ushort> block_chunk_forward_y;
+			[NoAlias] [ReadOnly] NativeArray<ushort> block_chunk_forward_z;
 			[NoAlias] [WriteOnly] Container6Dir<NativeParallelHashSet<int3>> rect_sets;
 
 			public void Execute()
 			{
 				int3 chunk_size = coordinate.size;
-				int x, y, z;
+
+				var x_f_block_buffer = default(ushort);
+				var y_f_block_buffer = new NativeArray<ushort>(coordinate.size.x, Allocator.Temp);
+				var z_f_block_buffer = new NativeArray<ushort>(coordinate.size_xy, Allocator.Temp);
 				//1. Gen quads
-				var quad_streams = new Container6Dir<NativeStream>
+				var quad_streams = new Container6Dir<NativeStream>()
 				{
 					plus_x = new(1, Allocator.Temp),
 					mnus_x = new(1, Allocator.Temp),
@@ -133,8 +150,7 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 					plus_z = new(1, Allocator.Temp),
 					mnus_z = new(1, Allocator.Temp)
 				};
-
-				var quad_streams_w = new Container6Dir<NativeStream.Writer>
+				var quad_streams_w = new Container6Dir<NativeStream.Writer>()
 				{
 					plus_x = quad_streams.plus_x.AsWriter(),
 					mnus_x = quad_streams.mnus_x.AsWriter(),
@@ -151,94 +167,43 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				quad_streams_w.plus_z.BeginForEachIndex(0);
 				quad_streams_w.mnus_z.BeginForEachIndex(0);
 
-
-				int i, i_x_f, i_y_f, i_z_f;
-				//1.1. Gen inside quads
-				for (z = 0; z < chunk_size.z - 1; z++)
+				for (int z = chunk_size.z - 1; 0 <= z; z--)
 				{
-					for (y = 0; y < chunk_size.y - 1; y++)
+					var part_z = z * coordinate.size_xy;
+					for (int y = chunk_size.y - 1; 0 <= y; y--)
 					{
-						for (x = 0; x < chunk_size.x - 1; x++)
+						var part_y = y * chunk_size.x;
+						for (int x = chunk_size.x - 1; 0 <= x; x--)
 						{
-							coordinate.To1D(x, y, z, out i);
-							coordinate.MoveX(i, 1, out i_x_f);
-							coordinate.MoveZ(i, 1, out i_y_f);
-							coordinate.MoveY(i, 1, out i_z_f);
-							var cur_inside = inside_chunk[i];
-							if (cur_inside)
+							var part_xy = x + part_y;
+							int i = part_xy + part_z;
+							var x_f_block = x != chunk_size.x - 1 ? x_f_block_buffer : block_chunk_forward_x[i - x];
+							var y_f_block = y != chunk_size.y - 1 ? y_f_block_buffer[x] : block_chunk_forward_y[i - part_y];
+							var z_f_block = z != chunk_size.z - 1 ? z_f_block_buffer[part_xy] : block_chunk_forward_z[part_xy];
+							var cur_block = block_chunk[i];
+							var cur_group = block_group_by_id[cur_block];
+							if (cur_group == inside_block_group)
 							{
-								var cur_id = volume_chunk[i];
-								if (!inside_chunk[i_x_f]) { quad_streams_w.plus_x.Write(new int2(cur_id, i)); }
-								if (!inside_chunk[i_y_f]) { quad_streams_w.plus_y.Write(new int2(cur_id, i)); }
-								if (!inside_chunk[i_z_f]) { quad_streams_w.plus_z.Write(new int2(cur_id, i)); }
+								if (block_group_by_id[x_f_block] != inside_block_group) { quad_streams_w.plus_x.Write(new int2(cur_block, i)); }
+								if (block_group_by_id[y_f_block] != inside_block_group) { quad_streams_w.plus_y.Write(new int2(cur_block, i)); }
+								if (block_group_by_id[z_f_block] != inside_block_group) { quad_streams_w.plus_z.Write(new int2(cur_block, i)); }
 							}
 							else
 							{
-								if (inside_chunk[i_x_f]) { quad_streams_w.mnus_x.Write(new int2(volume_chunk[i_x_f], i)); }
-								if (inside_chunk[i_y_f]) { quad_streams_w.mnus_y.Write(new int2(volume_chunk[i_y_f], i)); }
-								if (inside_chunk[i_z_f]) { quad_streams_w.mnus_z.Write(new int2(volume_chunk[i_z_f], i)); }
+								if (block_group_by_id[x_f_block] == inside_block_group) { quad_streams_w.mnus_x.Write(new int2(x_f_block, i)); }
+								if (block_group_by_id[y_f_block] == inside_block_group) { quad_streams_w.mnus_y.Write(new int2(y_f_block, i)); }
+								if (block_group_by_id[z_f_block] == inside_block_group) { quad_streams_w.mnus_z.Write(new int2(z_f_block, i)); }
 							}
+
+							z_f_block_buffer[part_xy] = cur_block;
+							y_f_block_buffer[x] = cur_block;
+							x_f_block_buffer = cur_block;
 						}
 					}
 				}
 
-				//1.2 Gen chunk edge quads
-				x = chunk_size.x - 1;
-				for (z = 0; z < chunk_size.z; z++)
-				{
-					for (y = 0; y < chunk_size.y; y++)
-					{
-						coordinate.To1D(x, y, z, out i);
-						coordinate.To1D(0, y, z, out i_x_f);
-						var cur_inside = inside_chunk[i];
-						if (cur_inside)
-						{
-							if (!inside_chunk_neighbor_x[i_x_f]) { quad_streams_w.plus_x.Write(new int2(volume_chunk[i], i)); }
-						}
-						else
-						{
-							if (inside_chunk_neighbor_x[i_x_f]) { quad_streams_w.mnus_x.Write(new int2(volume_chunk_neighbor_x[i_x_f], i)); }
-						}
-					}
-				}
-
-				y = chunk_size.y - 1;
-				for (z = 0; z < chunk_size.z; z++)
-				{
-					for (x = 0; x < chunk_size.x; x++)
-					{
-						coordinate.To1D(x, y, z, out i);
-						coordinate.To1D(x, 0, z, out i_y_f);
-						var cur_inside = inside_chunk[i];
-						if (cur_inside)
-						{
-							if (!inside_chunk_neighbor_y[i_y_f]) { quad_streams_w.plus_y.Write(new int2(volume_chunk[i], i)); }
-						}
-						else
-						{
-							if (inside_chunk_neighbor_y[i_y_f]) { quad_streams_w.mnus_y.Write(new int2(volume_chunk_neighbor_y[i_y_f], i)); }
-						}
-					}
-				}
-
-				z = chunk_size.z - 1;
-				for (y = 0; y < chunk_size.y; y++)
-				{
-					for (int x_zf = 0; x_zf < chunk_size.x; x_zf++)
-					{
-						coordinate.To1D(x_zf, y, z, out i);
-						coordinate.To1D(x_zf, y, 0, out i_z_f);
-						var cur_inside = inside_chunk[i];
-						if (cur_inside)
-						{
-							if (!inside_chunk_neighbor_z[i_z_f]) { quad_streams_w.plus_z.Write(new int2(volume_chunk[i], i)); }
-						}
-						else
-						{
-							if (inside_chunk_neighbor_z[i_z_f]) { quad_streams_w.mnus_z.Write(new int2(volume_chunk_neighbor_z[i_z_f], i)); }
-						}
-					}
-				}
+				y_f_block_buffer.Dispose();
+				z_f_block_buffer.Dispose();
 
 				quad_streams_w.plus_x.EndForEachIndex();
 				quad_streams_w.mnus_x.EndForEachIndex();
@@ -248,8 +213,6 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				quad_streams_w.mnus_z.EndForEachIndex();
 
 				//2. Greedy Mesh
-
-				//2.1 Greedy Line
 				ContainerListsToSets<int2>(quad_streams, out var quad_sets);
 				var line_sets = new Container6Dir<NativeParallelHashSet<int3>>
 				{
@@ -260,6 +223,7 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 					plus_z = new(1, Allocator.Temp),
 					mnus_z = new(1, Allocator.Temp)
 				};
+
 				Greedy_QuadSetToLineSet<X_Quad_Walker>(coordinate, quad_sets.plus_x, line_sets.plus_x);
 				Greedy_QuadSetToLineSet<X_Quad_Walker>(coordinate, quad_sets.mnus_x, line_sets.mnus_x);
 				Greedy_QuadSetToLineSet<Y_Quad_Walker>(coordinate, quad_sets.plus_y, line_sets.plus_y);
@@ -267,7 +231,6 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				Greedy_QuadSetToLineSet<Z_Quad_Walker>(coordinate, quad_sets.plus_z, line_sets.plus_z);
 				Greedy_QuadSetToLineSet<Z_Quad_Walker>(coordinate, quad_sets.mnus_z, line_sets.mnus_z);
 
-				//2.2 Greedy Rect
 				Greedy_LineSetToRectSet<X_Line_Walker>(coordinate, line_sets.plus_x, rect_sets.plus_x);
 				Greedy_LineSetToRectSet<X_Line_Walker>(coordinate, line_sets.mnus_x, rect_sets.mnus_x);
 				Greedy_LineSetToRectSet<Y_Line_Walker>(coordinate, line_sets.plus_y, rect_sets.plus_y);
@@ -275,37 +238,36 @@ namespace VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense
 				Greedy_LineSetToRectSet<Z_Line_Walker>(coordinate, line_sets.plus_z, rect_sets.plus_z);
 				Greedy_LineSetToRectSet<Z_Line_Walker>(coordinate, line_sets.mnus_z, rect_sets.mnus_z);
 
+				quad_streams.Dispose();
+				line_sets.Dispose();
+
 			}
 
-			public GenRectSetsJob(int3 chunk_size,
-				NativeArray<ushort> volume_chunk,
-				NativeArray<ushort> volume_chunk_neighbor_x,
-				NativeArray<ushort> volume_chunk_neighbor_y,
-				NativeArray<ushort> volume_chunk_neighbor_z,
-				NativeArray<bool> inside_chunk,
-				NativeArray<bool> inside_chunk_neighbor_x,
-				NativeArray<bool> inside_chunk_neighbor_y,
-				NativeArray<bool> inside_chunk_neighbor_z,
+			public GenRectSetsJob(
+				int inside_block_group, NativeArray<int> block_group_by_id,
+				int3 chunk_size,
+				NativeArray<ushort> block_chunk,
+				NativeArray<ushort> block_chunk_forward_x,
+				NativeArray<ushort> block_chunk_forward_y,
+				NativeArray<ushort> block_chunk_forward_z,
 				out Container6Dir<NativeParallelHashSet<int3>> rect_sets)
 			{
+				coordinate = new(chunk_size);
 				rect_sets = new()
 				{
-					plus_x = new(1, Allocator.TempJob),
-					mnus_x = new(1, Allocator.TempJob),
-					plus_y = new(1, Allocator.TempJob),
-					mnus_y = new(1, Allocator.TempJob),
-					plus_z = new(1, Allocator.TempJob),
-					mnus_z = new(1, Allocator.TempJob)
+					plus_x = new(1, Allocator.Persistent),
+					mnus_x = new(1, Allocator.Persistent),
+					plus_y = new(1, Allocator.Persistent),
+					mnus_y = new(1, Allocator.Persistent),
+					plus_z = new(1, Allocator.Persistent),
+					mnus_z = new(1, Allocator.Persistent)
 				};
-				coordinate = new(chunk_size);
-				this.volume_chunk = volume_chunk;
-				this.volume_chunk_neighbor_x = volume_chunk_neighbor_x;
-				this.volume_chunk_neighbor_y = volume_chunk_neighbor_y;
-				this.volume_chunk_neighbor_z = volume_chunk_neighbor_z;
-				this.inside_chunk = inside_chunk;
-				this.inside_chunk_neighbor_x = inside_chunk_neighbor_x;
-				this.inside_chunk_neighbor_y = inside_chunk_neighbor_y;
-				this.inside_chunk_neighbor_z = inside_chunk_neighbor_z;
+				this.inside_block_group = inside_block_group;
+				this.block_group_by_id = block_group_by_id;
+				this.block_chunk = block_chunk;
+				this.block_chunk_forward_x = block_chunk_forward_x;
+				this.block_chunk_forward_y = block_chunk_forward_y;
+				this.block_chunk_forward_z = block_chunk_forward_z;
 				this.rect_sets = rect_sets;
 			}
 		}
--- VolumeMegaStructure/Assets/Labs/TestGenVoxel/TestGenVoxelSequentialDense.cs
+++ VolumeMegaStructure/Assets/Labs/TestGenVoxel/TestGenVoxelSequentialDense.cs
@@ -2,11 +2,11 @@ using System.Linq;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense;
 using VolumeMegaStructure.Util;
 using static PrototypePackages.JobUtils.Template.IPlan;
 using static PrototypePackages.PrototypeUtils.AsyncUtil;
 using static Unity.Jobs.JobHandle;
-using static VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense.InsideGeneration;
 using static VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense.MeshGeneration;
 using static VolumeMegaStructure.Generate.ProceduralMesh.Voxel.SequentialDense.RectsGeneration;
 using static VolumeMegaStructure.Generate.Volume.Terrain.TestTerrainGeneration;
@@ -37,7 +37,7 @@ namespace Labs.TestGenVoxel
 			ScheduleBatchedJobs();
 			var terrain_jh_x = Plan<GenSimpleMountainTerrainJob>(
 				new(3, 4, 2, 1, 0, 140, 160,
-					new(256, 0),
+					new(chunk_size.x, 0),
 					new(0.001f, 0),
 					new(new(8, 0), new(250, 0), new(1000)),
 					new(new(60, 0), new(3, 0), new(1000)),
@@ -45,40 +45,63 @@ namespace Labs.TestGenVoxel
 			ScheduleBatchedJobs();
 			var terrain_jh_z = Plan<GenSimpleMountainTerrainJob>(
 				new(3, 4, 2, 1, 0, 140, 160,
-					new(0, 256),
+					new(0, chunk_size.z),
 					new(0.001f, 0),
 					new(new(8, 0), new(250, 0), new(1000)),
 					new(new(60, 0), new(3, 0), new(1000)),
 					chunk_size, out var volume_chunk_z_f));
 			ScheduleBatchedJobs();
 			var volume_chunk_y_f = new NativeArray<ushort>(Enumerable.Repeat((ushort)0, chunk_size.volume()).ToArray(), Allocator.Persistent);
+			var terran_jh = CombineDependencies(terrain_jh0, terrain_jh_x, terrain_jh_z);
 			w.Stop();
 
-			w.Start("Gen 4 inside chunks");
-			var inside_jh0 = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk, out var inside_chunk), terrain_jh0);
-			var inside_jh_x = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_x_f, out var inside_chunk_x_f), terrain_jh_x);
-			var inside_jh_z = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_z_f, out var inside_chunk_z_f), terrain_jh_z);
-			var jh_y = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_y_f, out var inside_chunk_y_f));
-			ScheduleBatchedJobs();
-			var inside_jh = CombineDependencies(inside_jh0, CombineDependencies(inside_jh_x, jh_y, inside_jh_z));
-			w.Stop();
+			// w.Start("Gen 4 inside chunks");
+			// var inside_jh0 = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk, out var inside_chunk), terrain_jh0);
+			// var inside_jh_x = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_x_f, out var inside_chunk_x_f), terrain_jh_x);
+			// var inside_jh_z = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_z_f, out var inside_chunk_z_f), terrain_jh_z);
+			// var jh_y = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_y_f, out var inside_chunk_y_f));
+			// ScheduleBatchedJobs();
+			// var inside_jh = CombineDependencies(inside_jh0, CombineDependencies(inside_jh_x, jh_y, inside_jh_z));
+			// w.Stop();
 
 			w.Start("ScanRectSets");
-			var rect_jh = Plan<GenRectSetsJob>(new(chunk_size,
+			var rect_jh = Plan<GenRectSetsJob>(new(
+				1, block_id_to_block_group,
+				chunk_size,
 				volume_chunk, volume_chunk_x_f, volume_chunk_y_f, volume_chunk_z_f,
-				inside_chunk, inside_chunk_x_f, inside_chunk_y_f, inside_chunk_z_f,
-				out var rect_sets), inside_jh);
+				out var rect_sets), terran_jh);
 			ScheduleBatchedJobs();
 			await WaitUntil(() => rect_jh.IsCompleted, 1);
 			rect_jh.Complete();
 			w.Stop();
 
 			w.Start("GenMesh");
-			var mesh = await GenMeshAsync(voxel_render_manager.max_index_array, chunk_size, rect_sets);
+			mesh = await GenMeshAsync(voxel_render_manager.max_index_array, chunk_size, rect_sets);
+			w.Stop();
+
+			w.Start("Dispose");
+			volume_chunk.Dispose();
+			volume_chunk_x_f.Dispose();
+			volume_chunk_y_f.Dispose();
+			volume_chunk_z_f.Dispose();
+			rect_sets.Dispose();
 			w.Stop();
 
 			Log(w.PrintAllRecords());
+
+			ApplyToMono();
 		}
+
+		void ApplyToMono()
+		{
+			var mesh_filter = GetComponent<MeshFilter>();
+			mesh_filter.sharedMesh = mesh;
+			var mesh_renderer = GetComponent<MeshRenderer>();
+			mesh_renderer.sharedMaterial = voxel_render_manager.material;
+		}
+
+		Mesh mesh;
+
 		void Start()
 		{
 			GenSingleOnce();
