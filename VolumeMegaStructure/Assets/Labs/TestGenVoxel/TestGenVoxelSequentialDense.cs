5f03fa6486667cced7e8e0bdc4d3a085a1edfff9
iamabigartist <pch4178@gmail.com>
2023-01-25 10:27:55 +0800

    Update TestGenVoxelSequentialDense.cs
    test batch timing and deps manage

--- VolumeMegaStructure/Assets/Labs/TestGenVoxel/TestGenVoxelSequentialDense.cs
+++ VolumeMegaStructure/Assets/Labs/TestGenVoxel/TestGenVoxelSequentialDense.cs
@@ -34,6 +34,7 @@ namespace Labs.TestGenVoxel
 					new(new(8, 0), new(250, 0), new(1000)),
 					new(new(60, 0), new(3, 0), new(1000)),
 					chunk_size, out var volume_chunk));
+			ScheduleBatchedJobs();
 			var terrain_jh_x = Plan<GenSimpleMountainTerrainJob>(
 				new(3, 4, 2, 1, 0, 140, 160,
 					new(256, 0),
@@ -41,6 +42,7 @@ namespace Labs.TestGenVoxel
 					new(new(8, 0), new(250, 0), new(1000)),
 					new(new(60, 0), new(3, 0), new(1000)),
 					chunk_size, out var volume_chunk_x_f));
+			ScheduleBatchedJobs();
 			var terrain_jh_z = Plan<GenSimpleMountainTerrainJob>(
 				new(3, 4, 2, 1, 0, 140, 160,
 					new(0, 256),
@@ -50,27 +52,22 @@ namespace Labs.TestGenVoxel
 					chunk_size, out var volume_chunk_z_f));
 			ScheduleBatchedJobs();
 			var volume_chunk_y_f = new NativeArray<ushort>(Enumerable.Repeat((ushort)0, chunk_size.volume()).ToArray(), Allocator.Persistent);
-			var terrain_jh = CombineDependencies(terrain_jh0, terrain_jh_x, terrain_jh_z);
-			await WaitUntil(() => terrain_jh.IsCompleted, 1);
-			terrain_jh.Complete();
 			w.Stop();
 
 			w.Start("Gen 4 inside chunks");
-			var inside_jh0 = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk, out var inside_chunk));
-			var inside_jh_x = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_x_f, out var inside_chunk_x_f));
-			var inside_jh_z = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_z_f, out var inside_chunk_z_f));
+			var inside_jh0 = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk, out var inside_chunk), terrain_jh0);
+			var inside_jh_x = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_x_f, out var inside_chunk_x_f), terrain_jh_x);
+			var inside_jh_z = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_z_f, out var inside_chunk_z_f), terrain_jh_z);
 			var jh_y = Plan<GenInsideJob>(new(1, block_id_to_block_group, volume_chunk_y_f, out var inside_chunk_y_f));
 			ScheduleBatchedJobs();
 			var inside_jh = CombineDependencies(inside_jh0, CombineDependencies(inside_jh_x, jh_y, inside_jh_z));
-			await WaitUntil(() => inside_jh.IsCompleted, 1);
-			inside_jh.Complete();
 			w.Stop();
 
 			w.Start("ScanRectSets");
 			var rect_jh = Plan<GenRectSetsJob>(new(chunk_size,
 				volume_chunk, volume_chunk_x_f, volume_chunk_y_f, volume_chunk_z_f,
 				inside_chunk, inside_chunk_x_f, inside_chunk_y_f, inside_chunk_z_f,
-				out var rect_sets));
+				out var rect_sets), inside_jh);
 			ScheduleBatchedJobs();
 			await WaitUntil(() => rect_jh.IsCompleted, 1);
 			rect_jh.Complete();
